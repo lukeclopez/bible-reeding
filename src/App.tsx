945f281f4d04bcfabcb61b86527e5cc126596b2a
Luke Lopez <xlucas14@gmail.com>
2020-02-08 18:46:07 -0500

    Extracted `VerseSelect`, wrote funcs to populate dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from "react";
 
-import { Row, Col } from "reactstrap";
-
-import MyDropdown from "./components/dropdown";
+import VerseSelect from "./components/verseSelect";
 import "./css/App.css";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Row>
-          <Col>
-            <MyDropdown title="Book" />
-          </Col>
-          <Col>
-            <MyDropdown title="Chapter" />
-          </Col>
-          <Col>
-            <MyDropdown title="Verse" />
-          </Col>
-        </Row>
+        <VerseSelect />
       </header>
     </div>
   );
---
+++ src/components/verseSelect.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Row, Col } from "reactstrap";
+
+import MyDropdown from "../components/dropdown";
+import book_chapters_verses from "../data/verses_and_chapters_per_book.json";
+import chapters_verses from "../data/verses_per_chapter.json";
+
+export interface VerseSelectProps {}
+
+const getBookNames = (): string[] => book_chapters_verses.map(b => b.name);
+const getBookChapters = (bookName: string): number[] => {
+  const book = book_chapters_verses.find(el => el.name === bookName);
+
+  const chapters = [];
+  for (let i = 1; i <= book!.chapters; i++) {
+    chapters.push(i);
+  }
+
+  return chapters;
+};
+const getVersesFor = (bookName: string, chapter: number): number[] => {
+  const book = chapters_verses.find(el => el.name === bookName);
+  const chaptersInBook = book!["17"];
+
+  const versesInChapter = [];
+  for (let i = 1; i <= chaptersInBook!; i++) {
+    versesInChapter.push(i);
+  }
+
+  return versesInChapter;
+};
+
+const VerseSelect: React.SFC<VerseSelectProps> = () => {
+  return (
+    <Row>
+      <Col>
+        <MyDropdown title="Book" options={getBookNames()} />
+      </Col>
+      <Col>
+        <MyDropdown title="Chapter" options={getBookChapters("Genesis")} />
+      </Col>
+      <Col>
+        <MyDropdown title="Verse" options={getVersesFor("Genesis", 1)} />
+      </Col>
+    </Row>
+  );
+};
+
+export default VerseSelect;
