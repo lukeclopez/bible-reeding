1df0a547e7a649e52d7261efdd89dc1a573a8253
Luke Lopez <xlucas14@gmail.com>
2020-02-11 19:06:47 -0500

    Initialized state with proper types

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,11 @@ import { main } from "./reducers";
 
 const initialState = {
   startBook: "Genesis",
-  startChapter: "1",
-  startVerse: "1",
+  startChapter: 1,
+  startVerse: 1,
   endBook: "Genesis",
-  endChapter: "5",
-  endVerse: "1"
+  endChapter: 1,
+  endVerse: 31
 };
 
 const store = createStore(main, initialState);
