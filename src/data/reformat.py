6408244a37b1cdeef031fb2eb769d528420f56a3
Luke Lopez <xlucas14@gmail.com>
2020-02-12 17:13:58 -0500

    Lumped all Bible data together into `book_data.json`

--- src/data/reformat.py
+++ src/data/reformat.py
@@ -2,20 +2,20 @@ import json
 
 from pprint import pprint
 
-# Format verses_per_chapter more logically
-with open("verses_and_chapters_per_book.json") as vpc:
+with open("verses_per_chapter.json") as vpc:
     verses_per_chapter = json.loads(vpc.read())
 
+with open("verses_and_chapters_per_book.json") as vpc:
+    verses_per_chapters_per_book = json.loads(vpc.read())
 
-def lift_name(input_dict):
-    name = input_dict["name"]
-    del input_dict["name"]
-    tup = (name, input_dict)
+copy_vpcpb = verses_per_chapters_per_book.copy()
+for book_name, chapters_verses in verses_per_chapters_per_book.items():
+    if book_name == "Joshua":
+        chapters = [18, 24, 17, 24, 15, 27, 26, 35, 27, 43, 23,
+                    24, 33, 15, 63, 10, 18, 28, 51, 9, 45, 34, 16, 33]
+    else:
+        chapters = [i for i in verses_per_chapter[book_name].values()]
+    copy_vpcpb[book_name]["versesPerChapter"] = chapters
 
-    return tup
-
-
-new_format_books = dict(map(lift_name, verses_per_chapter))
-
-with open("verses_and_chapters_per_book.json", "w") as vpc:
-    vpc.write(json.dumps(new_format_books))
+with open("book_data.json", "w") as vpc:
+    vpc.write(json.dumps(copy_vpcpb))
