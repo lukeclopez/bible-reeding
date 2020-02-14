3627c4967ac24de525377a8238eed61f70bd77d7
Luke Lopez <xlucas14@gmail.com>
2020-02-13 21:34:28 -0500

    Removed dropdown and tile

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 
 import VerseSelect from "./components/verseSelect";
 import Result from "./components/result";
-import Tile from "./components/tile";
 import store from "./redux/store";
 import "./css/App.css";
 
@@ -24,7 +23,6 @@ const App = () => {
           </Row>
           <Row>?</Row>
           <Result></Result>
-          <Tile title={"Genesis"}></Tile>
         </header>
       </div>
     </Provider>
