748cc96480832c43a869a37f95f0e92b1d7ebab1
Luke Lopez <xlucas14@gmail.com>
2020-02-14 11:12:36 -0500

    Included more directories in `tsconfig` and typed utils file

---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import bookData from "./data/bookData.json";
+
+const allBooks: { [index: string]: any } = bookData;
+
+export const range = (start: number, end: number): number[] => {
+  const arr = [];
+  for (var i = start; i < end; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+};
+
+export const countChaptersFor = (bookName: string): number => {
+  return allBooks[bookName].chapters;
+};
+
+export const countVersesFor = (bookName: string, chapter: number): number => {
+  // Subtract one to get the correct index of the chapter.
+  // Example: Chapter 1 has index 0.
+  return allBooks[bookName]["versesPerChapter"][chapter - 1];
+};
+
+export const getBookNames = () => Object.keys(allBooks);
